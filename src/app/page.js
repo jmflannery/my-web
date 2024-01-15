24aaf23c213e826955fcbbc8bb7ea4364b96805b
Jack Flannery <hello@jackflannery.com>
2024-01-15 08:57:06 -0500

    Fetch profile and render with MDXRemote

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,24 @@
 import Link from 'next/link';
 import Image from 'next/image';
+import {MDXRemote} from 'next-mdx-remote/rsc';
 import SocialismIcom from '../../public/socialism.svg';
 import links from '../links';
 
 import './page.css';
 
-const Home = () => {
+const getProfile = async () => {
+  const res = await fetch(process.env.API_URL);
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch Profile');
+  }
+
+  return res.json();
+};
+
+const Home = async () => {
+  const profile = await getProfile();
+
   return (
     <main>
       <div>
@@ -19,8 +32,8 @@ const Home = () => {
         </div>
         <div className="header">
           <div>
-            <h1>Jack Flannery</h1>
-            <p>Full Stack Web Developer</p>
+            <h1>{profile.name}</h1>
+            <p>{profile.title}</p>
           </div>
         </div>
         <div className="links">
@@ -35,34 +48,7 @@ const Home = () => {
         </div>
         <div className="content">
           <div>
-            <p>
-              Hi, I&#39;m Jack Flannery. I&#39;m a girl dad and full stack web
-              developer. I live in New Jersey with my wife Danielle and our
-              daughter Olivia. Besides spending time with my family I enjoy
-              tinkering with computer software, and riding various boards: snow,
-              skate, wake, surf, etc. Our favorite thing to do is travel,
-              we&#39;ve spent a lot of time on the road and in the sky. Trying
-              to see as much of this earth as possible before our journey ends.
-            </p>
-            <p>
-              I&#39;m a full stack web developer at Braidio. We&#39;re making
-              some really interesting products and we use some pretty neat tools
-              like React, React Native, Redux and Ruby on Rails. I&#39;ve always
-              been drawn to computer programming I&#39;m very lucky to be able
-              to do something I enjoy for a living. Braidio is a fully remote
-              company so I can work from wherever I want. You can find me
-              working at{' '}
-              <a href="https://highlandscowork.com/">Highlands Cowork</a> or
-              maybe in that corner of my bedroom that I call my office, or
-              perhaps at some National Park inside a rental car. It&#39;s hard
-              to know for sure.
-            </p>
-            <p>
-              I&#39;m usually working on various side projects. You can find my
-              blog here. I mostly write web development tutorials on topics that
-              I&#39;m currenty working on, or any topics I&#39;m interested in
-              and don&#39;t get to work with at my day job.
-            </p>
+            <MDXRemote source={profile.bio} />
           </div>
         </div>
         <div className="footer">
