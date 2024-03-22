60d622c97ecb52e8082a48ab820e12e84a64ad6f
Jack Flannery <hello@jackflannery.com>
2024-03-22 13:26:21 -0400

    Basicaly the same code

--- src/app/blog/route.js
+++ src/app/blog/route.js
@@ -1,9 +1,10 @@
 import {redirect} from 'next/navigation';
-import {fetchLatestPost} from '@/actions/posts';
+import {fetchPosts} from '@/actions/posts';
 import urls from '@/urls';
 
 export const GET = async _request => {
-  const post = await fetchLatestPost();
+  const posts = await fetchPosts();
+  const post = posts?.[0];
 
   if (post?.published_at) {
     redirect(urls.web.blog.post(post.slug));
