66d0611e7dbf312ee13b9310eb8b426f45b3cd66
Jack Flannery <hello@jackflannery.com>
2024-03-22 16:05:23 -0400

    Add title and meta description to blog post pages

--- src/app/blog/posts/[slug]/page.js
+++ src/app/blog/posts/[slug]/page.js
@@ -2,6 +2,23 @@ import {notFound} from 'next/navigation';
 import {fetchPost, fetchPosts} from '@/actions/posts';
 import Post from '@/components/Post';
 
+const blogName = "Jack's Weblog";
+
+const withBlogName = text => `${text} | ${blogName}`;
+
+export const generateMetadata = async ({params: {slug}}) => {
+  const posts = await fetchPosts();
+  const postItem = posts.find(post => post.slug === slug);
+  const post = await fetchPost(postItem?.id);
+
+  if (!post) return {};
+
+  return {
+    title: withBlogName(post.title),
+    description: withBlogName(`${post.title} | ${post.description}`),
+  };
+};
+
 export const getStaticParams = () =>
   fetchPosts().map(post => {
     slug: post.slug;
